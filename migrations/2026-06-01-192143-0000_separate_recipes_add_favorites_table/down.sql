53a570b11e8e8aba4581cec278624c220dce50ea
Ian Pratt <ianjdpratt@gmail.com>
2026-06-01 20:34:04 +0100

    fix down.sql stuffs

--- migrations/2026-06-01-192143-0000_separate_recipes_add_favorites_table/down.sql
+++ migrations/2026-06-01-192143-0000_separate_recipes_add_favorites_table/down.sql
@@ -1,3 +1,3 @@
 -- This file should undo anything in `up.sql`
-ALTER TABLE "recipes" ADD "user_id" INT4 NOT NULL; 
+ALTER TABLE "recipes" ADD "user_id" INT4 NOT NULL DEFAULT 0; 
 DROP TABLE IF EXISTS "users_favorite_recipes";
